1aa83efe17b5c888359bb38000e4b77a4013ffab
Christian Cruz <seseko98@gmail.com>
2022-01-31 21:02:30 -0600

    Created scene

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sum: UILabel!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        for button in 0...11 {
+            
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
