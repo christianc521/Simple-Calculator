31b3064d8e904078ed2a0ed33f70b4ae314aa823
Christian Cruz <seseko98@gmail.com>
2022-01-31 21:09:04 -0600

    added logic

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var total = 0
     @IBOutlet weak var sum: UILabel!
-    
     @IBAction func buttonPressed(_ sender: UIButton) {
-        for button in 0...11 {
-            
+        if (sender.tag >= 1 || sender.tag <= 9) {
+            sum.text = "\(sender.tag)"
         }
     }
     override func viewDidLoad() {
